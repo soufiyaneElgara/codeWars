b37f7339b842baa5c3e831f22e4fc231868e0507
soufiyaneElgara <soufiyaneelgara@gmail.com>
2025-04-08 01:06:55 +0100

    claculating the volume Of Cubiod using a static method to a class

---
+++ volumeOfCuboid.js
@@ -0,0 +1,11 @@
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+        return length * width * height;
+    
+    }
+}
+    
+
+
+    
+  console.log(Kata.getVolumeOfCuboid(1, 2, 2));
